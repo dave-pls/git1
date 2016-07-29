179953386de80bd2ad5fc2969f12a9c8f65f0c79
dave-pls <dave@positivelendingsolutions.com.au>
2016-07-29 16:57:58 +1000

    added basic html elements to index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+
+
+<!DOCTYPE html>
+<html lang=en>
+	
+<head>
+	<title>Git1</title>
+	
+	<meta name="description" content="This is Git1 Test">
+	
+</head>
+
+<body>
+	
 <h1>THis Heading was added via GitHub editor</h1>
 <h2>Github h2 ssssss</h2>
-Halo! This is a test for <?php echo 'git1'; ?>.app
+<p>Halo! This is a test for <?php echo 'git1'; ?>.app</p>
+
+</body>
+</html>
