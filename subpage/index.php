be95ebe5480776e0bb8d3f2b708bd09476bce613
dave-pls <dave@positivelendingsolutions.com.au>
2016-07-29 17:00:58 +1000

    added subpage dir and file

---
+++ subpage/index.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html lang=en>
+	
+<head>
+	<title>Git1</title>
+	
+	<meta name="description" content="This is Git1 Test">
+	
+</head>
+
+<body>
+	
+<h1>Sub Page Index</h1>
+<p>Halo! This is a test for <?php echo 'git1'; ?>.app</p>
+
+</body>
+</html>
